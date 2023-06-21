ab66159e1fd49d1815eef50a5641a1dde5717ffb
Nikita Agarwal <115351051+nikita-1310@users.noreply.github.com>
2023-06-21 23:15:56 +0530

    Add files via upload

---
+++ userInput.js
@@ -0,0 +1,9 @@
+// Take user Input from console
+const prompt = require("prompt-sync")();
+var c= prompt("Enter You Name ")
+console.log("Your Name is "+c);
+
+
+// Take user input only number
+var num= Number(prompt("Enter Number"))
+console.log("The exponantial of number is "+(num**2));
